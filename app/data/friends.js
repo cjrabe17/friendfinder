f971cffdab887e4fe1f2e19a7156588f6295ba5f
Cassie <cjrabe17@gmail.com>
2018-02-17 14:58:25 -0500

    Added basic routes & dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendArray = [
+    {
+        "name":"Ahmed",
+        "photo":"http://cutecatshq.com/wp-content/uploads/2014/05/Ozzy-Dating-Profile-Photo.-Hello-Ladies.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
+
+module.exports = friendArray;
